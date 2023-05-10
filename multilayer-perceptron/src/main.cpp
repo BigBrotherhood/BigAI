b481e68bf8b7a4562e549b71dd2d296aceb511e2
mbastard <87230146+mbastard42@users.noreply.github.com>
2023-05-10 20:21:56 +0200

    prompt2

--- multilayer-perceptron/src/main.cpp
+++ multilayer-perceptron/src/main.cpp
@@ -1,12 +1,31 @@
 #include "../inc/multilayer-perceptron.hpp"
 
-int main() {
+void training() {
 
     std::string path;
 
     std::cout << "Please enter path to database: "; std::cin >> path;
-
+    
     dataset data(path);
 
+}
+
+void predict() {
+    
+    std::cout << "work in progress" << std::endl;
+}
+
+
+int main() {
+
+    int choice = 2;
+
+    while (choice != 0 && choice != 1) {
+        std::cout << "Please choose between training phase (0) and prediction phase (1): "; std::cin >> choice;
+    }
+    if (choice)
+        predict();
+    else
+        training();
     return 0;
 }
